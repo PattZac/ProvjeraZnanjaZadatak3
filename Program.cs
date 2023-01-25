e1f6b05ef402bea0e71f9b6b597ae4073445b938
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-25 08:56:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvjeraZnanjaZadatak3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fibonaccijev = 0, Fibonaccijevm1 = 1, Fibonaccijevm2 = 0;
+            Console.Write("Unesi broj: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<n; i++)
+            {
+                Fibonaccijev = Fibonaccijevm1 + Fibonaccijevm2;
+                Fibonaccijevm2 = Fibonaccijevm1;
+                Fibonaccijevm1 = Fibonaccijev;
+            }
+            if (n == 1) { Fibonaccijev = 1; }
+            Console.WriteLine(Fibonaccijev);
+            Console.ReadLine();
+        }
+    }
+}
